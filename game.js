4b13e17c2a446a28d8a48a9fbeb1ffcf01a427fb
KurtaVova <46198068+KurtaVova@users.noreply.github.com>
2019-01-02 18:01:21 +0200

    Add files via upload
    добавил базовые классы игры: Vector, Actor и Level.

--- game.js
+++ game.js
@@ -1,1 +1,146 @@
 'use strict';
+
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vector) {
+    if (!(vector instanceof Vector)) {
+      throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+    }
+    return new Vector(this.x + vector.x, this.y + vector.y);
+  }
+
+  times(factor) {
+    return new Vector(this.x * factor, this.y * factor);
+  }
+}
+
+
+
+class Actor {
+    constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+        if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
+            throw new Error();
+        }
+        this.pos = pos;
+        this.size = size;
+        this.speed = speed;
+    }
+
+    act() {}
+
+    get left() {
+        return this.pos.x;
+    }
+
+    get top() {
+        return this.pos.y;
+    }
+
+    get right() {
+        return this.pos.x + this.size.x;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y;
+    }
+
+    get type() {
+        return 'actor';
+    }
+
+    isIntersect(anObject) {
+        if (anObject === undefined || !(anObject instanceof Actor)) {
+            throw new Error();
+        }
+
+        if (anObject === this) {
+            return false;
+        }
+
+        if (this.left >= anObject.right || this.top >= anObject.bottom || anObject.left >= this.right || anObject.top >= this.bottom) {
+            return false;
+        } else return true;
+    }
+}
+
+
+
+class Level {
+    constructor(grid = [], actors = []) {
+      this.grid = grid;
+      this.actors = actors;
+      this.height = grid.length;
+      this.player = actors.find(actor => actor.type === 'player');
+      this.width = Math.max(0, ...(this.grid.map(item => item.length)));
+      this.status = null;
+      this.finishDelay = 1;
+    }
+  
+    isFinished() {
+        if (this.status !== null && this.finishDelay < 0) {
+            return true;
+        } else return false;
+    }
+  
+    actorAt(willReturn) {
+        if (willReturn === undefined || !(willReturn instanceof Actor)) {
+            throw new Error();
+        }
+
+        return this.actors.find(function(x) { return x.isIntersect(willReturn)});
+    }
+  
+    obstacleAt(pos, size) {
+      const left = Math.floor(pos.x);
+      const right = Math.ceil(pos.x + size.x);
+      const top = Math.floor(pos.y);
+      const bottom = Math.ceil(pos.y + size.y);
+      if (!(pos instanceof Vector) || !(size instanceof Vector)) {
+        throw new Error('Объект должен быть типа Vector');
+      }
+  
+      if (left < 0 || right > this.width || top < 0) {
+        return 'wall';
+      }
+      if (bottom > this.height) {
+        return 'lava';
+      }
+      for (let y = top; y < bottom; y++) {
+        for (let x = left; x < right; x++) {
+          const gridBlock = this.grid[y][x];
+          if (gridBlock) {
+            return gridBlock;
+          }
+        }
+      }
+    }
+  
+    removeActor(actor) {
+      const index = this.actors.indexOf(actor);
+      if (index != -1) {
+        this.actors.splice(index, 1);
+      }
+    }
+  
+    noMoreActors(type) {
+      return !this.actors.some((actor) => actor.type === type)
+    }
+  
+    playerTouched(type, actor) {
+      if (['lava', 'fireball'].some((block) => block === type)) {
+        return this.status = 'lost';
+      }
+      if (type === 'coin' && actor.type === 'coin') {
+        this.removeActor(actor);
+        if (this.noMoreActors('coin')) {
+          return this.status = 'won'
+        }
+      }
+    }
+  }
+
+  
